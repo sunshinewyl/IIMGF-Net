4dde5d9faa13e437940500eade8a8277042b7101
sunshinewyl <wuyli29@mail2.sysu.edu.cn>
2025-05-17 17:29:04 +0800

    Add files via upload

---
+++ models/aux_model/mamba/function_last.py
@@ -0,0 +1,173 @@
+import os
+import time
+import math
+import copy
+from functools import partial
+from typing import Optional, Callable, Any
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+from einops import rearrange, repeat
+from timm.layers import DropPath, trunc_normal_
+from fvcore.nn import FlopCountAnalysis, flop_count_str, flop_count, parameter_count
+DropPath.__repr__ = lambda self: f"timm.DropPath({self.drop_prob})"
+
+# import mamba_ssm.selective_scan_fn (in which causal_conv1d is needed)
+try:
+    from mamba_ssm.ops.selective_scan_interface import selective_scan_fn
+except:
+    pass
+
+# an alternative for mamba_ssm
+# try:
+#     from selective_scan_interface import selective_scan_fn as selective_scan_fn_v1
+# except:
+#     pass
+
+# cross selective scan ===============================
+# if True:
+    # import selective_scan_cuda_core as selective_scan_cuda
+
+
+class Cross_Mamba_Attention_SSM(nn.Module):
+    def __init__(
+            self,
+            # basic dims ===========
+            d_model=96,
+            d_state=4,
+            ssm_ratio=1,
+            dt_rank="auto",
+            # dt init ==============
+            dt_min=0.001,
+            dt_max=0.1,
+            dt_init="random",
+            dt_scale=1.0,
+            dt_init_floor=1e-4,
+            # ======================
+            **kwargs,
+    ):
+        factory_kwargs = {"device": None, "dtype": None}
+        super().__init__()
+        self.d_model = d_model
+        self.d_state = math.ceil(self.d_model / 6) if d_state == "auto" else d_state  # 20240109
+        self.expand = ssm_ratio
+        self.d_inner = int(self.expand * self.d_model)
+        self.dt_rank = math.ceil(self.d_model / 16) if dt_rank == "auto" else dt_rank
+
+        # x proj; dt proj ============================
+        self.x_proj_1 = nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs)
+        self.x_proj_2 = nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs)
+
+        self.dt_proj_1 = self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                                    **factory_kwargs)
+        self.dt_proj_2 = self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                                    **factory_kwargs)
+
+        # A, D =======================================
+        self.A_log_1 = self.A_log_init(self.d_state, self.d_inner)  # (D, N)
+        self.A_log_2 = self.A_log_init(self.d_state, self.d_inner)  # (D)
+        self.D_1 = self.D_init(self.d_inner)  # (D)
+        self.D_2 = self.D_init(self.d_inner)  # (D)
+
+        # out norm ===================================
+        self.out_norm_1 = nn.LayerNorm(self.d_inner)
+        self.out_norm_2 = nn.LayerNorm(self.d_inner)
+
+    @staticmethod
+    def dt_init(dt_rank, d_inner, dt_scale=1.0, dt_init="random", dt_min=0.001, dt_max=0.1, dt_init_floor=1e-4,
+                **factory_kwargs):
+        dt_proj = nn.Linear(dt_rank, d_inner, bias=True, **factory_kwargs)
+
+        # Initialize special dt projection to preserve variance at initialization
+        dt_init_std = dt_rank ** -0.5 * dt_scale
+        if dt_init == "constant":
+            nn.init.constant_(dt_proj.weight, dt_init_std)
+        elif dt_init == "random":
+            nn.init.uniform_(dt_proj.weight, -dt_init_std, dt_init_std)
+        else:
+            raise NotImplementedError
+
+        # Initialize dt bias so that F.softplus(dt_bias) is between dt_min and dt_max
+        dt = torch.exp(
+            torch.rand(d_inner, **factory_kwargs) * (math.log(dt_max) - math.log(dt_min))
+            + math.log(dt_min)
+        ).clamp(min=dt_init_floor)
+        # Inverse of softplus: https://github.com/pytorch/pytorch/issues/72759
+        inv_dt = dt + torch.log(-torch.expm1(-dt))
+        with torch.no_grad():
+            dt_proj.bias.copy_(inv_dt)
+        # Our initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
+        # dt_proj.bias._no_reinit = True
+
+        return dt_proj
+
+    @staticmethod
+    def A_log_init(d_state, d_inner, copies=-1, device=None, merge=True):
+        # S4D real initialization
+        A = repeat(
+            torch.arange(1, d_state + 1, dtype=torch.float32, device=device),
+            "n -> d n",
+            d=d_inner,
+        ).contiguous()
+        A_log = torch.log(A)  # Keep A_log in fp32
+        if copies > 0:
+            A_log = repeat(A_log, "d n -> r d n", r=copies)
+            if merge:
+                A_log = A_log.flatten(0, 1)
+        A_log = nn.Parameter(A_log)
+        A_log._no_weight_decay = True
+        return A_log
+
+    @staticmethod
+    def D_init(d_inner, copies=-1, device=None, merge=True):
+        # D "skip" parameter
+        D = torch.ones(d_inner, device=device)
+        if copies > 0:
+            D = repeat(D, "n1 -> r n1", r=copies)
+            if merge:
+                D = D.flatten(0, 1)
+        D = nn.Parameter(D)  # Keep in fp32
+        D._no_weight_decay = True
+        return D
+
+    def forward(self, x_rgb: torch.Tensor, x_e: torch.Tensor):
+        selective_scan = selective_scan_fn
+        B, d, L = x_rgb.shape
+        # x_rgb = x_rgb.permute(0, 2, 1)
+        # x_e = x_e.permute(0, 2, 1)
+        x_dbl_rgb = self.x_proj_1(rearrange(x_rgb, "b d l -> (b l) d"))  # (bl d)
+        x_dbl_e = self.x_proj_2(rearrange(x_e, "b d l -> (b l) d"))  # (bl d)
+        dt_rgb, B_rgb, C_rgb = torch.split(x_dbl_rgb, [self.dt_rank, self.d_state, self.d_state], dim=-1)
+        dt_e, B_e, C_e = torch.split(x_dbl_e, [self.dt_rank, self.d_state, self.d_state], dim=-1)
+        dt_rgb = self.dt_proj_1.weight @ dt_rgb.t()
+        dt_e = self.dt_proj_2.weight @ dt_e.t()
+        dt_rgb = rearrange(dt_rgb, "d (b l) -> b d l", l=L)
+        dt_e = rearrange(dt_e, "d (b l) -> b d l", l=L)
+        A_rgb = -torch.exp(self.A_log_1.float())  # (k * d, d_state)
+        A_e = -torch.exp(self.A_log_2.float())  # (k * d, d_state)
+        B_rgb = rearrange(B_rgb, "(b l) dstate -> b dstate l", l=L).contiguous()
+        B_e = rearrange(B_e, "(b l) dstate -> b dstate l", l=L).contiguous()
+        C_rgb = rearrange(C_rgb, "(b l) dstate -> b dstate l", l=L).contiguous()
+        C_e = rearrange(C_e, "(b l) dstate -> b dstate l", l=L).contiguous()
+
+        y_rgb = selective_scan(
+            x_rgb, dt_rgb,
+            A_rgb, B_rgb, C_e, self.D_1.float(),
+            delta_bias=self.dt_proj_1.bias.float(),
+            delta_softplus=True,
+        )
+        y_e = selective_scan(
+            x_e, dt_e,
+            A_e, B_e, C_rgb, self.D_2.float(),
+            delta_bias=self.dt_proj_2.bias.float(),
+            delta_softplus=True,
+        )
+        # assert out_y.dtype == torch.float
+        y_rgb = rearrange(y_rgb, "b d l -> b l d")
+        y_rgb = self.out_norm_1(y_rgb)
+        y_e = rearrange(y_e, "b d l -> b l d")
+        y_e = self.out_norm_2(y_e)
+        return y_rgb, y_e
